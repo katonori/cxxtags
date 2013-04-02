1877856520146112de5b8fd829d67efc9bcf76d3
katonori <katonori.d@gmail.com>
2013-04-03 01:11:27 +0900

    [test] added test for single quoting in SQL query.

---
+++ test/template_type_param/main.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+#include <iostream>
+
+class C0 {
+public:
+    C0(int a) : mVal(a) {}
+    void check(void) const
+    {
+        printf("check: %d\n", mVal);
+    }
+private:
+    int mVal;
+};
+
+template <typename in_type >
+void func_test(in_type val)
+{
+    std::cout << "out: " << val << std::endl;
+}
+
+int main()
+{
+    func_test(1);
+    func_test(2.2);
+    std::vector<C0 > vecTest;
+    for(int i = 0; i < 8; i++) {
+        vecTest.push_back(C0(i));
+    }
+    for(std::vector<C0 >::iterator itr = vecTest.begin();
+        itr != vecTest.end();
+        itr++) {
+        itr->check();
+    }
+    return 0;
+}

e430cbe78e314094c2df7cde11a47084e5270222
katonori <katonori.d@gmail.com>
2014-07-08 00:06:18 +0900

    updated wrapper scripts. skip indexing when option "-MM" is specified.

--- src/g++.sh
+++ src/g++.sh
@@ -3,6 +3,10 @@
 if [ "${CXXTAGS_CXX}" = "" ]; then
     CXXTAGS_CXX=g++
 fi
+
+MATCH=`echo $* | grep -- "-MM" | wc -l`
+if [ ${MATCH} -eq 0 ]; then
+    cxxtags -p `echo $* | perl -pne 's/ -o\s+(\S+)[ ]*/ -o \1.tagdb /g'`
+fi
 ${CXXTAGS_CXX} $*
-cxxtags -p `echo $* | perl -pne 's/ -o\s+(\S+)[ ]*/ -o \1.tagdb /g'`
 exit 0
--- src/gcc.sh
+++ src/gcc.sh
@@ -3,7 +3,10 @@
 if [ "${CXXTAGS_CC}" = "" ]; then
     CXXTAGS_CC=gcc
 fi
+MATCH=`echo $* | grep -- "-MM" | wc -l`
+if [ ${MATCH} -eq 0 ]; then
+    cxxtags -p `echo $* | perl -pne 's/ -o\s+(\S+)[ ]*/ -o \1.tagdb /g'`
+fi
 ${CXXTAGS_CC} $*
-cxxtags -p `echo $* | perl -pne 's/ -o\s+(\S+)[ ]*/ -o \1.tagdb /g'`
 exit 0
 

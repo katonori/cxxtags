74589453744bc7226f8eb2ef7b3af88f754042a3
katonori <katonori.d@gmail.com>
2014-07-08 02:16:16 +0900

    changed the wrapper scripts to be able to specify exclude list by the environment variable.

--- src/g++.sh
+++ src/g++.sh
@@ -4,9 +4,13 @@ if [ "${CXXTAGS_CXX}" = "" ]; then
     CXXTAGS_CXX=g++
 fi
 
-MATCH=`echo $* | grep -- "-MM" | wc -l`
+if [ "${CXXTAGS_EXCLUDE}" != "" ]; then
+    CXXTAGS_EXCLUDE=" -e ${CXXTAGS_EXCLUDE}"
+fi
+
+MATCH=`echo $* | grep -- "-MM " | wc -l`
 if [ ${MATCH} -eq 0 ]; then
-    cxxtags -p `echo $* | perl -pne 's/ -o\s+(\S+)[ ]*/ -o \1.tagdb /g'`
+    cxxtags -p ${CXXTAGS_EXCLUDE} `echo $* | perl -pne 's/ -o\s+(\S+)[ ]*/ -o \1.tagdb /g'`
 fi
 ${CXXTAGS_CXX} $*
 exit 0
--- src/gcc.sh
+++ src/gcc.sh
@@ -3,9 +3,14 @@
 if [ "${CXXTAGS_CC}" = "" ]; then
     CXXTAGS_CC=gcc
 fi
-MATCH=`echo $* | grep -- "-MM" | wc -l`
+
+if [ "${CXXTAGS_EXCLUDE}" != "" ]; then
+    CXXTAGS_EXCLUDE=" -e ${CXXTAGS_EXCLUDE}"
+fi
+
+MATCH=`echo $* | grep -- "-MM " | wc -l`
 if [ ${MATCH} -eq 0 ]; then
-    cxxtags -p `echo $* | perl -pne 's/ -o\s+(\S+)[ ]*/ -o \1.tagdb /g'`
+    cxxtags -p ${CXXTAGS_EXCLUDE} `echo $* | perl -pne 's/ -o\s+(\S+)[ ]*/ -o \1.tagdb /g'`
 fi
 ${CXXTAGS_CC} $*
 exit 0

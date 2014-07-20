a0108e7433c014e7a9ab3234c18c61713d630fe7
katonori <katonori.d@gmail.com>
2014-07-20 16:26:55 +0900

    added wrapper scripts.

---
+++ src/g++.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+import os, sys
+import getopt
+import datetime
+
+CXXTAGS = "cxxtags"
+CXXTAGS_DB_MANAGER = "cxxtags_db_manager"
+CXXTAGS_OPT = "-E -p"
+
+db_dst = ""
+if "CXXTAGS_DB_DST" in os.environ:
+    db_dst = os.environ["CXXTAGS_DB_DST"]
+else:
+    print "ERROR: set tag database directory CXXTAGS_DB_DST to generate tag database"
+    sys.exit(1)
+
+compiler = "g++"
+if "CXXTAGS_CXX" in os.environ:
+    compiler = os.environ["CXXTAGS_CXX"]
+exclude = ""
+if "CXXTAGS_EXCLUDE" in os.environ:
+    exclude = os.environ["CXXTAGS_EXCLUDE"]
+if exclude != "":
+    exclude = "-e " + exclude
+
+# get output filename
+output = None
+i = 0
+argv = sys.argv[1:]
+while 1:
+    if i >= len(argv):
+        break
+    arg = argv[i]
+    if arg == "-o":
+        output = argv[i+1]
+        del argv[i:i+2]
+        i += 2
+    else:
+        i += 1
+if output == None:
+    print "could not determine output file: " + " ".join(argv)
+    # generate output filename
+    dt = str(datetime.datetime.now())
+    dt = dt.replace(" ", "_")
+    output = dt.replace(":", "_") + "." + str(os.getpid()) + ".db"
+    print "use random name: "  + output
+if not "-MM" in argv:
+    args_tmp = [CXXTAGS, CXXTAGS_OPT, exclude, " ".join(argv), " -o " + output]
+    # run cxxtags
+    cmd = " ".join(args_tmp)
+    print "CXXTAGS: " + cmd
+    os.system(cmd)
+    args_tmp = [CXXTAGS_DB_MANAGER, "add", db_dst, output]
+    cmd = " ".join(args_tmp)
+    print "CXXTAGS: " + cmd
+    os.system(cmd)
+print argv
+
+# run compilation
+exit(os.system(compiler + " " + " ".join(argv[1:])))

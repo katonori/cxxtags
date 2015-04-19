2a1e2eedb2a43f2357b81db736a35aa761699fcc
katonori <katonori.d@gmail.com>
2015-04-20 02:02:59 +0900

    Change tests to use llvm-config to obtain build options.

--- test/util/common.mk
+++ test/util/common.mk
@@ -1,5 +1,5 @@
 CXXTAGS = $(PROJ_ROOT)/bin/cxxtags -e /usr/include
-CXXTAGS_INCLUDES = -I${LLVM_HOME}/lib/clang/3.4/include
+CXXTAGS_INCLUDES = `llvm-config --cxxflags`
 CXXTAGS_DB_MANAGER = $(PROJ_ROOT)/bin/cxxtags_db_manager
 CXXTAGS_QUERY = $(PROJ_ROOT)/bin/cxxtags_query
 DUMPER = $(PROJ_ROOT)/bin/cxxtags_html_dumper
--- test/util/common_rule.mk
+++ test/util/common_rule.mk
@@ -1,8 +1,2 @@
-check:
-ifeq (${LLVM_HOME},)
-	@echo "ERROR: set LLVM_HOME"
-	@exit 1
-endif
-
 .cpp.db:
 	$(CXXTAGS) $(CXXTAGS_INCLUDES) db $< 

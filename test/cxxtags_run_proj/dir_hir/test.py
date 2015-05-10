3766bf53152444a633898200898bc8ad033f0378
katonori <katonori.d@gmail.com>
2015-05-10 09:45:09 +0900

    * Change a command execution not to execute command via a shell. * Fix wrong argument handling.

--- test/cxxtags_run_proj/dir_hir/test.py
+++ test/cxxtags_run_proj/dir_hir/test.py
@@ -5,10 +5,10 @@ import sys
 result = "func|" + os.path.abspath(os.path.dirname(__file__)) + "/b/b.h|3|20|static inline void func()"
 
 commands.getstatusoutput("rm -rf _db")
-(rv, out) = commands.getstatusoutput("cxxtags_run_proj -t vs _db proja.sln")
+(rv, out) = commands.getstatusoutput("../../../bin/cxxtags_run_proj -t vs _db proja.sln")
 assert rv == 0
 print out
-(rv, out) = commands.getstatusoutput("cxxtags_query def _db a/a.cpp 10 12")
+(rv, out) = commands.getstatusoutput("../../../bin/cxxtags_query def _db a/a.cpp 10 12")
 assert rv == 0
 print out
 assert out == result
